54d228b9187a7ba7e989c808c93309a959174ee1
Andrew Miller <andrew.patrick.miller.2022@gmail.com>
2022-12-24 14:23:12 -0500

    added logic

--- projects/redo-list/src/app/core/services/current-list.service.ts
+++ projects/redo-list/src/app/core/services/current-list.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { CompletedListService } from './completed-list.service';
 
 /**
  * This is for managing the redo lists state
@@ -6,15 +7,61 @@ import { Injectable } from '@angular/core';
  */
 @Injectable({
   providedIn: 'root',
-  redoList: RedoList;
 })
 export class CurrentListService {
-  
-  constructor() {}
+  redoList: RedoList = {
+    name: 'Redo List',
+    todos: [{ name: 'Brush Teeth', isFinished: false }],
+    isFinished: false,
+    date: new Date().toString(),
+  };
+
+  constructor(public completedListService: CompletedListService) {}
+  addTodo(todo: Todo) {
+    this.redoList.todos.push(todo);
+  }
+  removeTodo(name: Todo['name']) {
+    const targetsIndex = this.redoList.todos.findIndex(
+      (currentTodo) => name === currentTodo.name
+    );
+    if (targetsIndex >= 0) {
+      this.redoList.todos.splice(targetsIndex, 1);
+    }
+  }
+  markTodoComplete(name: Todo['name']) {
+    const targetTodo = this.redoList.todos.find((todo) => todo.name === name);
+    if (targetTodo) {
+      targetTodo.isFinished = true;
+      if (this.redoList.todos.every((todo) => todo.isFinished)) {
+        this.markListComplete();
+      }
+    }
+  }
+  markTodoIncomplete(name: Todo['name']) {
+    const targetTodo = this.redoList.todos.find((todo) => todo.name === name);
+    if (targetTodo) {
+      targetTodo.isFinished = false;
+      if (this.redoList.isFinished) {
+        this.markListIncomplete();
+      }
+    }
+  }
+
+  markListComplete() {
+    this.redoList.isFinished = true;
+  }
+  markListIncomplete() {
+    this.redoList.isFinished = false;
+  }
 }
 
-type RedoList = {
+export type RedoList = {
   name: 'Redo List';
-
-}
-
+  todos: Todo[];
+  isFinished: boolean;
+  date: string;
+};
+export type Todo = {
+  name: string;
+  isFinished: boolean;
+};
