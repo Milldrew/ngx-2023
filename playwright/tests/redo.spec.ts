29869dc1ec6c1d5882ea471a7303a8a3385de394
Andrew Miller <andrew.patrick.miller.2022@gmail.com>
2022-12-26 19:03:44 -0500

    wrote first playwright visual test

--- playwright/tests/redo.spec.ts
+++ playwright/tests/redo.spec.ts
@@ -1,20 +1,12 @@
 import { test, expect } from '@playwright/test';
+const LOCAL = 'http://localhost:4200';
+const SUBMIT_REDO = '#submit-redo-button';
+const CREATE_REDO = '#create-redo-button';
 
 test('homepage has title and links to intro page', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-
-  // create a locator
-  const getStarted = page.getByRole('link', { name: 'Get started' });
-
-  // Expect an attribute "to be strictly equal" to the value.
-  await expect(getStarted).toHaveAttribute('href', '/docs/intro');
-
-  // Click the get started link.
-  await getStarted.click();
-
-  // Expects the URL to contain intro.
-  await expect(page).toHaveURL(/.*intro/);
+  await page.goto(LOCAL);
+  //====== Test Submit
+  await page.click(CREATE_REDO);
+  await page.click(SUBMIT_REDO);
+  await page.waitForTimeout(60000 * 20);
 });
