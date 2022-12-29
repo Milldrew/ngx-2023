3fc7f84cf615570dda13a6a62559c711acc04454
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-12-29 17:33:33 -0500

    deleted some redundant code

--- projects/scratch/src/app/pie-chart.directive.ts
+++ projects/scratch/src/app/pie-chart.directive.ts
@@ -1,39 +1,5 @@
-import { Directive, ElementRef } from '@angular/core';
-import * as d3 from 'd3';
+import { Directive } from '@angular/core';
 import { PieChartBaseDirective } from './pie-chart-base.directive';
-const MOCK_DATA = [
-  {
-    name: 'A',
-    value: 3,
-  },
-  {
-    name: 'B',
-    value: 5,
-  },
-  {
-    name: 'C',
-    value: 2,
-  },
-  {
-    name: 'D',
-    value: 8,
-  },
-  {
-    name: 'E',
-    value: 8,
-  },
-  {
-    name: 'F',
-    value: 8,
-  },
-  {
-    name: 'G',
-    value: 8,
-  },
-];
-type Data = { name: string; value: number };
-const getNames = (data: Data) => data.name;
-const getValues = (data: Data) => data.value;
 
 /** Exports a diretive that takes the id of the element that it is assigned to
  *  The Pie Chart has a title at the top and each slice of the pie chart has a label and the value
